028992cd08eae2137edcb0fe5d9d2850dc2ecb2b
Felipe dos Santos Bispo <felipesbispO@gmail.com>
2025-08-11 19:06:03 -0300

    feat: create Product entity with stock management methods

---
+++ src/StockControl.Domain/Entities/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockControl.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int MinimumStock { get; set; }
+
+        public Product(string name, int minimumStock)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            MinimumStock = minimumStock;
+            Quantity = 0;
+        }
+
+        public void AddStock(int quantity)
+        {
+            if (quantity <= 0)
+                throw new ArgumentException("Quantity must be greater than zero.", nameof(quantity));
+            
+            Quantity += quantity;
+        }
+
+        public void RemoveStock(int quantity)
+        {
+            if (quantity <= 0)
+                throw new ArgumentException("Quantity must be greater than zero.", nameof(quantity));
+            if (quantity > Quantity)
+                throw new InvalidOperationException("Insufficient stock to remove the specified quantity.");
+            Quantity -= quantity;
+        }
+
+        public bool IsBelowMinimumStock()
+        {
+            return Quantity < MinimumStock;
+        }
+    }
+}
